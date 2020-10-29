852700cd1a0e06e5702474315f62d79ce18a48dd
mmalak7 <marcinmalak7@gmail.com>
2020-10-29 13:34:36 +0100

    display header with banner

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import Banner from "./components/banner";
+import Header from "./components/header";
+import "./styles/App.scss";
+
+function App() {
+  
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
