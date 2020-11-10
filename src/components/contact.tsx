2fe86446f436c425fa9089d0f03cddb64716831b
mmalak7 <marcinmalak7@gmail.com>
2020-11-10 22:15:29 +0100

    styling for contact & about page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,7 +2,25 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>this is contact page</div>
+    <div className="container-contact">
+      <div className="left-panel">
+        <div className="header-panel">
+          <div className="upper-part">
+            <p className="collaborate">Let's collaborate!</p>
+            <p className="email-part">miostudio@gmail.com</p>
+          </div>
+        </div>
+        <div className="contact-panel">
+          <p>Katowice, Poland</p>
+          <p>(+48) 123 456 789</p>
+        </div>
+      </div>
+      <div className="right-panel">
+        <div className="dog-pic">
+          <img src={process.env.PUBLIC_URL + `suzi.jpg`} alt="" />
+        </div>
+      </div>
+    </div>
   );
 };
 
